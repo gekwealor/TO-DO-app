8a51d4cc11cb14835e3dd0af3c8859f6b93227ce
gekwealor <gekwealor@gmail.com>
2024-01-22 14:30:31 -0500

    Project

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a readme file for my to-do app project. HTML, CSS, and JS was used for it to effectively work. 
---
+++ index.js
@@ -0,0 +1,37 @@
+const inputBox = document.getElementById("input-box");
+const containerlist = document.getElementById("container-list");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Something must be written");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        containerlist.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+containerlist.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", containerlist.innerHTML);
+}
+function viewTask(){
+    containerlist.innerHTML = localStorage.getItem("data");
+}
+viewTask();
